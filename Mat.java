f499a7843dc7e638cda3dbb7ce048133c85617e1
nandana-24 <nandana.2k04@gmail.com>
2026-01-21 15:32:35 +0530

    matrix addition

---
+++ Mat.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Mat{
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.err.println("enter no of rows: ");
+        int r=s.nextInt();
+        System.err.println("enter no of rows: ");
+        int c=s.nextInt();
+        int m1[][] = new int[r][c];
+        int m2[][]=new int[r][c];
+        int m3[][]=new int[r][c];
+        System.out.println("enter elements of first matrix: ");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                m1[i][j]=s.nextInt();
+            }
+        }
+        System.out.println("enter elements of 2nd matrix: ");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                m2[i][j]=s.nextInt();
+            }
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                m3[i][j]=m1[i][j]+m2[i][j];
+            }
+        }
+        System.out.println("New matrix: ");
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                System.out.print(m3[i][j]  + " ");
+            }
+            System.out.println();
+        }
+    }
+}
